d6980f73d5b1fcb7b778c113377186cb44c16ba6
jutaga <juancho9615@hotmail.com>
2022-11-14 14:48:20 -0500

    Cotizador con reactjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}" //Todas las carpetas con las extenciones
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
